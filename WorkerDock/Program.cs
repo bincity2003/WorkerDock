52a3cc1caa8f5b31a20c296ee847e1b7b012e4e9
bincity2003 <bincity2003@gmail.com>
2019-09-02 21:05:39 +0700

    Finising up program loop

--- WorkerDock/Program.cs
+++ WorkerDock/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using WorkerDock.Plugins;
 
 namespace WorkerDock
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static string User;
+        private static string Domain;
+        private static string Prompt => $"{User}@{Domain} > ";
+
+        private static Dictionary<string, PluginObject> Plugins;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            User = "user";
+            Domain = "SPMC";
+            LoadPlugins();
+
+            bool executionContinue = true;
+            string[] command;
+            string callee;
+            string[] parameters;
+            while (executionContinue)
+            {
+                Console.Write(Prompt);
+                command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (command.Length == 0)
+                {
+                    continue;
+                }
+                callee = command[0];
+                parameters = command[1..^0];
+
+                try
+                {
+                    Plugins[callee].Run(parameters[0], parameters[1..^0]);
+                }
+                catch (KeyNotFoundException)
+                {
+                    Plugins[""].Run(callee, parameters);
+                }
+            }
+        }
+
+        private static void LoadPlugins()
+        {
+            Plugins = new Dictionary<string, PluginObject>();
+
+            BasicPlugin basic = new BasicPlugin();
+            Plugins.Add(basic.InvokeID, basic);
         }
     }
 }
