48e8bd63d33ad658924abf86e528b2099e365fef
bincity2003 <bincity2003@gmail.com>
2019-09-02 20:25:48 +0700

    Create PluginObject.cs

---
+++ WorkerDock.Plugins/PluginObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerDock.Plugins
+{
+    public abstract class PluginObject
+    {
+        public abstract string Name { get; }
+        public abstract string Version { get; }
+        public abstract string InvokeID { get; }
+
+        public abstract void Startup(string[] args);
+    }
+}
