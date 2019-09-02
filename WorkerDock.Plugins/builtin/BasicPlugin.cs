4b7e3772597bc6a4b6b55a87724cc3b975a71830
bincity2003 <bincity2003@gmail.com>
2019-09-02 20:36:07 +0700

    Create BasicPlugin.cs
    Implemented basic properties and methods.

---
+++ WorkerDock.Plugins/builtin/BasicPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerDock.Plugins.builtin
+{
+    public sealed class BasicPlugin : PluginObject
+    {
+        public override string Name => "BasicPlugin";
+        public override string Version => "1.0.0";
+        public override string InvokeID => "";
+        public override string[] CallableCommand { get; }
+
+        public BasicPlugin()
+        {
+            CallableCommand = new string[]
+            {
+                "exit",
+                "clean",
+                "config"
+            };
+        }
+
+        public override void Run(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
