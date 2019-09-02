c8e89e835c38f7c9c55de206a7e2b041ff4a1b8f
bincity2003 <bincity2003@gmail.com>
2019-09-02 20:18:09 +0700

    Add project files.

---
+++ WorkerDock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkerDock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
