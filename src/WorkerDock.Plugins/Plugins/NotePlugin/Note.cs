4f2731d20927a831b30e9672ef3dc7041a1a5634
bincity2003 <bincity2003@gmail.com>
2019-09-03 18:00:54 +0700

    Create Note.cs

---
+++ src/WorkerDock.Plugins/Plugins/NotePlugin/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerDock.Plugins.Component.Private
+{
+    public struct Note
+    {
+        public string Index;
+        public DateTime Date;
+        public string Content;
+    }
+}
